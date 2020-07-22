32838ca0618ef808c9e6d0e18b7fdd8dfb9140bc
GKAtwood <gabrielle.k85@gmail.com>
2020-07-22 10:13:46 -0600

    setup

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express =require('express')
+const massive = require('massive');
+const session = require('express-session');
+
+
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env,
+
+app = express();
+
+app.use(express.json());
+
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  }))
+
+  massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then(db=>{
+    app.set('db',db);
+    console.log('db connected')
+});
+
+app.listen(SERVER_PORT, ()=> console.log(`Listening on port ${SERVER_PORT}`))
