c19110c9681068800ce53bd9044df8f72a8919c9
GKAtwood <gabrielle.k85@gmail.com>
2020-07-31 15:49:50 -0600

    commit

--- src/Components/Viewer.js
+++ src/Components/Viewer.js
@@ -1,9 +1,8 @@
   
 import React, { Component } from 'react';
-// import './App.css';
 import axios from 'axios';
 import {Link} from 'react-router-dom';
-// import {connect} from 'react-redux';
+
 
 
 
